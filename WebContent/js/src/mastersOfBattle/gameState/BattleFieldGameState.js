cc288f00ff37eb0d701194269258126b0e7fb829
unknown <abodnare@ABodnarescu.rom.europe.techteam.com>
2016-01-15 15:16:25 +0200

    a

---
+++ WebContent/js/src/mastersOfBattle/gameState/BattleFieldGameState.js
@@ -0,0 +1,8 @@
+var MASTERS_OF_BATTLE = MASTERS_OF_BATTLE || {};
+
+/**
+ * @constructor
+ */
+MASTERS_OF_BATTLE.BattleFieldGameState = function(unitStats, unitCharacteristics) {
+	
+};
---
+++ WebContent/js/src/mastersOfBattle/gameState/UnitGameState.js
@@ -0,0 +1,9 @@
+var MASTERS_OF_BATTLE = MASTERS_OF_BATTLE || {};
+
+/**
+ * @constructor
+ */
+MASTERS_OF_BATTLE.UnitGameState = function(unitStats, unitCharacteristics) {
+	this.unitStats = unitStats;
+	this.unitCharacteristics = unitCharacteristics;
+};
