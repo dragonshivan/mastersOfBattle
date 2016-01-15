d71aa78b65608a77286ccb4587a8f4dc2eec0729
unknown <abodnare@ABodnarescu.rom.europe.techteam.com>
2016-01-15 14:37:38 +0200

    a

--- WebContent/js/src/gameLoop/GameLoop.js
+++ WebContent/js/src/gameLoop/GameLoop.js
@@ -6,9 +6,10 @@ var GAME_LOOP = GAME_LOOP = GAME_LOOP || {};
  * @param {GAME_LOOP.Queue} inputEventQueue
  * @param {Number} canvasWidth
  * @param {Number} canvasHeight
+ * @param {Boolean} displayFPS
  * @constructor
  */
-GAME_LOOP.GameLoop = function(gameEntities, inputEventQueue, canvasWidth, canvasHeight) {
+GAME_LOOP.GameLoop = function(gameEntities, inputEventQueue, canvasWidth, canvasHeight, showFPS) {
 	
 	window.requestAnimationFrame = 
 			window.requestAnimationFrame || /* Firefox 23 / IE 10 / Chrome */
@@ -28,16 +29,27 @@ GAME_LOOP.GameLoop = function(gameEntities, inputEventQueue, canvasWidth, canvas
 	this.canvas.width=canvasWidth;
 	this.canvas.height=canvasHeight;
 	
+	this.showFPS = showFPS === undefined ? false: showFPS;
+	
 	this.context = this.canvas.getContext('2d');
 	
 	this.lastLoopCallTime = 0;
 	this.accumulatedTimeMs = 0;
+	
+	this.FPSLocalIntervalSeconds = 2;
+	this.framesCountLocal = 0;
+	this.framesCountLocalStartTimeMs = 0;
+	this.framesCountGlobal = 0;
+	this.framesCountGlobalStartTimeMs = 0;
+	this.FPSLocal = GAME_LOOP.DESIRED_FPS;
+	this.FPSGlobal = GAME_LOOP.DESIRED_FPS
 };
 
 /**
  * @public
  */
 GAME_LOOP.GameLoop.prototype.start = function() {
+	this.initFPSMeasurement();
 	this.lastLoopCallTime = this.getCurrentTimeMs();
 	this.update();
 };
@@ -57,7 +69,7 @@ GAME_LOOP.GameLoop.prototype.update = function() {
 		self.accumulatedTimeMs -= GAME_LOOP.FIXED_STEP_IDEAL_DURATION_MS;
 	}
 	
-	self.updateGraphics();
+	self.updateGraphics();	
 	
 	window.requestAnimationFrame(function() {self.update();});
 };
@@ -81,6 +93,7 @@ GAME_LOOP.GameLoop.prototype.updateState = function() {
 	for(var i = 0; i < this.gameEntities.length; i++) {
 		this.gameEntities[i].updateState();
 	}	
+	this.updateFPS();
 };
 
 /**
@@ -92,6 +105,10 @@ GAME_LOOP.GameLoop.prototype.updateGraphics = function() {
 	this.gameEntities.sort(this.gameEntityZIndexSort);
 	for(var i=0; i<this.gameEntities.length; i++) {
 		this.gameEntities[i].updateGraphics(this.context);
+	}
+	
+	if(this.showFPS) {
+		this.displayFPS();
 	}
 };	
 
@@ -111,4 +128,52 @@ GAME_LOOP.GameLoop.prototype.gameEntityZIndexSort = function(gameEntity1, gameEn
 		return gameEntity1.y - gameEntity2.y;
 	}
 	return val;
+};
+
+/**
+ * @private
+ */
+GAME_LOOP.GameLoop.prototype.initFPSMeasurement = function() {
+	this.framesCountLocalStartTimeMs = this.getCurrentTimeMs();
+	this.framesCountGlobalStartTimeMs = this.getCurrentTimeMs();
+};
+
+/**
+ * @private
+ */
+GAME_LOOP.GameLoop.prototype.updateFPS = function() {
+	var currentTimeMs = this.getCurrentTimeMs();
+	
+	var passedTimeLocalSeconds = (currentTimeMs - this.framesCountLocalStartTimeMs) / 1000;
+	if(passedTimeLocalSeconds >= this.FPSLocalIntervalSeconds) {
+		this.FPSLocal = this.framesCountLocal / passedTimeLocalSeconds;
+		this.framesCountLocal = 0;
+		this.framesCountLocalStartTimeMs = currentTimeMs;
+	}
+	this.framesCountLocal++;
+	
+	var passedTimeGlobalSeconds = (currentTimeMs - this.framesCountGlobalStartTimeMs) / 1000;
+	if(passedTimeGlobalSeconds != 0) {
+		this.FPSGlobal =this.framesCountGlobal / passedTimeGlobalSeconds;
+	}
+	this.framesCountGlobal++;
+};
+
+/**
+ * @private
+ */
+GAME_LOOP.GameLoop.prototype.displayFPS = function() {
+	this.context.fillStyle = 'rgba(225,225,225,0.75)';
+	this.context.fillRect(0,0,83,15);
+	
+	this.context.fillStyle = "black";	
+	this.context.font = "11px Arial";
+	this.context.fillText("FPS: " + this.round1Decimal(this.FPSLocal) + " | " + this.round1Decimal(this.FPSGlobal), 2, 12);
+};
+
+/**
+ * @private
+ */
+GAME_LOOP.GameLoop.prototype.round1Decimal = function(number) {
+	return parseFloat(Math.round(number * 100) / 100).toFixed(1);
 };
